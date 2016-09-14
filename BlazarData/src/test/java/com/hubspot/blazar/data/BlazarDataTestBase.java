9b0aa20b9a87213577edf5d5e52b4939c5f3a2eb
Jonathan Goodwin <jgoodwin@hubspot.com>
2016-09-14 10:54:37 -0400

    function for debugging arbitrary sql in ide

--- BlazarData/src/test/java/com/hubspot/blazar/data/BlazarDataTestBase.java
+++ BlazarData/src/test/java/com/hubspot/blazar/data/BlazarDataTestBase.java
@@ -1,7 +1,10 @@
 package com.hubspot.blazar.data;
 
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.BeforeClass;
+import org.skife.jdbi.v2.Query;
 
 import com.google.inject.Guice;
 import com.hubspot.blazar.test.base.service.BlazarTestBase;
@@ -21,5 +24,9 @@ public class BlazarDataTestBase extends BlazarTestBase {
   @After
   public void cleanup() throws Exception {
     runSql("schema.sql");
+  }
+
+   public Query<Map<String, Object>> runRawSql(String query) {
+    return getFromGuice(org.skife.jdbi.v2.DBI.class).open().createQuery(query);
   }
 }
